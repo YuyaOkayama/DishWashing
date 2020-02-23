09046f5f357f9e370405b0456cf6b8d275150506
YuyaOkayama <0eb3mc1878x4w5h@gmail.com>
2020-02-23 19:16:40 +0900

    tmpFile

--- app/controllers/wash_controller.rb
+++ app/controllers/wash_controller.rb
@@ -4,7 +4,7 @@ class WashController < ApplicationController
 	def initialize
 		super
 		begin
-			@wash_data = JSON.parse(File.read("data.txt"))
+			@wash_data = JSON.parse(File.read("/tmp/data.txt"))
 		rescue
 			@wash_data = Hash.new
 		@wash_data.each do |key,obj|
@@ -12,7 +12,7 @@ class WashController < ApplicationController
 				@wash_data.delete(key)
 			end
 		end
-		File.write("data,txt", @wash_data.to_json)
+		File.write("/tmp/data.txt", @wash_data.to_json)
 	end
 
 	def index
@@ -20,7 +20,7 @@ class WashController < ApplicationController
 			obj = MyData.new(name:params['name'])
 			@wash_data[Time.now.to_i] = obj
 			data = @wash_data.to_json
-			File.write("data.txt", data)
+			File.write("/tmp/data.txt", data)
 			@wash_data = JSON.parse(data)
 		end
 		@wash_count = @wash_data.length.to_s
